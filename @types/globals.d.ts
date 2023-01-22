1c01860760fcb296e21456e06067fa5825f2cfa7
Alderian Annatar <53636+Alderian@users.noreply.github.com>
2023-01-22 17:18:00 -0300

    Added prettier and solhint

--- @types/globals.d.ts
+++ @types/globals.d.ts
@@ -1,14 +1,14 @@
 declare namespace NodeJS {
-  export interface ProcessEnv {
-    ETHERSCAN_API_KEY: string
-    /** @see https://docs.alchemy.com/docs/alchemy-quickstart-guide#1key-create-an-alchemy-key */
-    NEXT_PUBLIC_ALCHEMY_API_KEY: string
-    /** Your EOA Private Key */
-    PRIVATE_KEY: string
-    /**
-     * Force to set Goerli read only provider for #getContract
-     * @see https://nextjs.org/docs/basic-features/environment-variables#exposing-environment-variables-to-the-browser
-     */
-    NEXT_PUBLIC_FORCE_GOERLI_PROVIDER: string
-  }
+    export interface ProcessEnv {
+        ETHERSCAN_API_KEY: string;
+        /** @see https://docs.alchemy.com/docs/alchemy-quickstart-guide#1key-create-an-alchemy-key */
+        NEXT_PUBLIC_ALCHEMY_API_KEY: string;
+        /** Your EOA Private Key */
+        PRIVATE_KEY: string;
+        /**
+         * Force to set Goerli read only provider for #getContract
+         * @see https://nextjs.org/docs/basic-features/environment-variables#exposing-environment-variables-to-the-browser
+         */
+        NEXT_PUBLIC_FORCE_GOERLI_PROVIDER: string;
+    }
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,46 +1,50 @@
-import type { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "hardhat-deploy"
-import "dotenv/config";
+import type {HardhatUserConfig} from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import 'hardhat-deploy';
+import '@nomiclabs/hardhat-solhint';
+import 'dotenv/config';
 
 const ALCHEMY_API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY;
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "0".repeat(64);
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '0'.repeat(64);
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      live: false,
-      saveDeployments: true,
-      tags: ["test", "local"],
-      chainId: 1337,
-      // chainId: 31337,
+    solidity: '0.8.17',
+    defaultNetwork: 'hardhat',
+    networks: {
+        localhost: {
+            live: false,
+            saveDeployments: true,
+            tags: ['local'],
+        },
+        hardhat: {
+            live: false,
+            saveDeployments: true,
+            tags: ['test', 'local'],
+        },
+        goerli: {
+            url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+            accounts: [PRIVATE_KEY],
+            live: true,
+            saveDeployments: true,
+            tags: ['staging'],
+            deploy: ['testnet-deploy/'],
+        },
     },
-    goerli: {
-      url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-      accounts: [PRIVATE_KEY],
-      live: true,
-      saveDeployments: true,
-      tags: ["staging"],
-      deploy: [ 'testnet-deploy/' ]
+    namedAccounts: {
+        deployer: 0,
     },
-  },
-  namedAccounts: {
-    deployer: 0,
-  },
-  gasReporter: {
-    enabled: COINMARKETCAP_API_KEY !== undefined,
-    currency: "USD",
-    coinmarketcap: COINMARKETCAP_API_KEY,
-  },
-  etherscan: {
-    apiKey: {
-      goerli: ETHERSCAN_API_KEY,
+    gasReporter: {
+        enabled: COINMARKETCAP_API_KEY !== undefined,
+        currency: 'USD',
+        coinmarketcap: COINMARKETCAP_API_KEY,
     },
-  },
+    etherscan: {
+        apiKey: {
+            goerli: ETHERSCAN_API_KEY,
+        },
+    },
 };
 
 export default config;
